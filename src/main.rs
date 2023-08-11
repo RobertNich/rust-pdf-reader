bbe617e3b82543a4a0ad9026180a57d81e354ccd
RobertNich <robert@rivalbet.com.au>
2023-08-11 23:35:25 +0930

    added text and contents extraction by pages

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,31 @@ use std::thread::sleep;
 use std::time::Duration;
 
 fn main() {
-    let path = "/test.pdf";
-    let text = extract_text_from_pdf(path).unwrap_or_else(|err| {
-        eprintln!("Error extracting text: {}", err);
-        String::new()
+    let (text_by_page, contents) = extract_text_and_contents_from_pdf(path).unwrap_or_else(|err| {
+        eprintln!("Error extracting: {}", err);
+        (HashMap::new(), String::new())
     });
 
-    display_text_word_by_word(&text, 200);
+    // Need to add GUI intergration here
+    if let Some(first_page_text) = text_by_page.get(&1) {
+        display_text_word_by_word(first_page_text, 200);
+    }
+}
+
+fn extract_text_and_contents_from_pdf(path: &str) -> Result<(HashMap<u32, String>, String), pdf_extract::Error> {
+    let mut extractor = Extractor::new(path, Settings::default())?;
+    let mut text_by_page = HashMap::new();
+    let mut contents = String::new();
+
+    for page in 1..=extractor.get_page_count()? {
+        let text = extractor.get_text_from_page(page)?;
+        if text.contains("Contents") || text.contains("Chapters") {
+            contents = text;
+        }
+        text_by_page.insert(page, text);
+    }
+
+    Ok((text_by_page, contents))
 }
 
 fn extract_text_from_pdf(path: &str) -> Result<String, pdf_extract::Error> {
